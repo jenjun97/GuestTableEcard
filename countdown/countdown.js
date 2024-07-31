5e06f3cefb60a69dca5a1ab17b6e2d67340b5366
jun <jenjun97@gmail.com>
2024-07-31 17:48:34 +0800

    完成上傳照片功能

--- countdown/countdown.js
+++ countdown/countdown.js
@@ -1,19 +1,22 @@
 // 設定倒數計時的目標日期和時間
-//var targetDate = new Date('2024-10-19T12:00:00');
-var targetDate = new Date('2024-07-30T17:35:00');
+var targetDate = new Date('2024-08-01T10:35:00');
 var timerId = null;
 
 // 格式化時間顯示
 function formatTime(time) {
-	// 設置計時器的字體顏色為黑色
-	document.getElementById('timer').style.color = 'black';
-	// 計算天數、小時、分鐘和秒數
-	var days = Math.floor(time / (3600 * 24));
-	var hours = Math.floor((time % (3600 * 24)) / 3600);
-	var minutes = Math.floor((time % 3600) / 60);
-	var seconds = time % 60;
-	// 返回格式化後的時間字串
-	return days + ':' + ('0' + hours).slice(-2) + ':' + ('0' + minutes).slice(-2) + ':' + ('0' + seconds).slice(-2);
+    // 設置計時器的字體顏色為黑色
+    document.getElementById('timer').style.color = 'black';
+    // 計算總小時數
+    var hours = Math.floor(time / 3600);
+    var minutes = Math.floor((time % 3600) / 60);
+    var seconds = time % 60;
+    // 返回格式化後的時間字串
+    // 格式化小時數，刪除千位數前面的0
+    var formattedHours = hours.toString();
+    
+    // 返回格式化後的時間字串
+    return formattedHours + ':' + ('0' + minutes).slice(-2) + ':' + ('0' + seconds).slice(-2);
+
 }
 
 // 更新計時器
