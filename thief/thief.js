613b5f365d97016a34cf47d5f790f39a340f15c7
jenjun <jenjun97@gmail.com>
2024-08-10 11:33:54 +0800

    增加怪盜顯示結果

--- thief/thief.js
+++ thief/thief.js
@@ -1,52 +1,61 @@
 document.getElementById('ps-btn').addEventListener('click', function() {
-    // 取得四個 input 的值
-    var passA = document.getElementById('pass-a').value.trim();
-    var passB = document.getElementById('pass-b').value.trim();
-    var passC = document.getElementById('pass-c').value.trim();
-    var passD = document.getElementById('pass-d').value.trim();
+	// 顯示 loading 畫面
+//	document.getElementById('loadingOverlay').style.display = 'flex';
+	document.getElementById('unlock-card').style.display = 'flex';
+	// 取得四個 input 的值
+	var passA = document.getElementById('pass-a').value.trim();
+	var passB = document.getElementById('pass-b').value.trim();
+	var passC = document.getElementById('pass-c').value.trim();
+	var passD = document.getElementById('pass-d').value.trim();
 
-    // 檢查每個 input 是否只有一個數字
-    if (!isValidNumber(passA) || !isValidNumber(passB) || !isValidNumber(passC) || !isValidNumber(passD)) {
-        alert("密碼內容有錯，請檢查");
-        return;
-    }
+	// 檢查每個 input 是否只有一個數字
+	if (!isValidNumber(passA) || !isValidNumber(passB) || !isValidNumber(passC) || !isValidNumber(passD)) {
+		alert("密碼內容有錯，請檢查");
+		return;
+	}
 
-    // 將四個數字組成字串
-    var password = passA + passB + passC + passD;
+	// 將四個數字組成字串
+	var password = passA + passB + passC + passD;
 
-    // 發送 POST 請求
-    fetch('http://localhost:8080/api/unlock_password', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ password: password })
-    })
-    .then(response => response.text())
-    .then(url => {
-        if (!url || url === 'null') {
-            alert("密碼錯誤");
-        } else {
-			// 導頁
-//            window.location.href = url;
+	// 發送 POST 請求
+	fetch('http://localhost:8080/api/thief_password', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({ password: password })
+	})
+		.then(response => response.json())
+		.then(data => {
+			const url = data.url;
+			const ansNum = data.ansNum;
 
-			// 清空輸入框內容
-			document.getElementById('pass-a').value = '';
-			document.getElementById('pass-b').value = '';
-			document.getElementById('pass-c').value = '';
-			document.getElementById('pass-d').value = '';
+			// 隱藏 loading 畫面
+			document.getElementById('loadingOverlay').style.display = 'none';
+			if (!url || url === 'null') {
+				alert("密碼錯誤");
+			} else {
+				// 導頁
+				//   window.location.href = url;
 
-			// 在新標籤頁中打開 URL
-			window.open(url, '_blank');
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        alert("發生錯誤，請告知婚宴聯絡人");
-    });
+				// 清空輸入框內容
+				document.getElementById('pass-a').value = '';
+				document.getElementById('pass-b').value = '';
+				document.getElementById('pass-c').value = '';
+				document.getElementById('pass-d').value = '';
+
+				// 在新標籤頁中打開 URL
+				// window.open(url, '_blank');
+				alert(url);
+			}
+		})
+		.catch(error => {
+			console.error('Error:', error);
+			alert("發生錯誤，請告知婚宴聯絡人");
+		});
 });
 
 // 檢查 input 的值是否是一個數字且只有一個字元
 function isValidNumber(value) {
-    return value.length === 1 && /^[0-9]$/.test(value);
+	return value.length === 1 && /^[0-9]$/.test(value);
 }
