a18991311835935267aa24801d6bd6e628dd31af
jun <jenjun97@gmail.com>
2024-09-12 22:04:24 +0800

    完成：怪盜解密

--- thief/thief.js
+++ thief/thief.js
@@ -1,3 +1,4 @@
+// 送出解鎖
 document.getElementById('unlock-btn').addEventListener('click', function() {
 	// 顯示 loading 畫面
 	document.getElementById('loadingOverlay').style.display = 'flex';
@@ -36,8 +37,12 @@ document.getElementById('unlock-btn').addEventListener('click', function() {
 
 			if (unlock === 'fail') {
 				document.getElementById('fail-card').style.display = 'flex';
+			} else if (unlock == 'winner') {
+				document.getElementById('winner-card').style.display = 'flex';
+				document.getElementById("winner-num").textContent = num;
+				document.getElementById("winner-uuid").value = data.uuid;
 			} else if (unlock == 'success') {
-				document.getElementById("num").textContent = num;
+				document.getElementById("success-num").textContent = num;
 				document.getElementById('success-card').style.display = 'flex';
 				document.getElementById("successUrl").href = url;
 			} else {
@@ -46,24 +51,24 @@ document.getElementById('unlock-btn').addEventListener('click', function() {
 			}
 
 			return;
-//			if (unlock === 'fail') {
-//				document.getElementById('loadingOverlay').style.display = 'flex';
-//			} else if (!url || url === 'null') {
-//				alert("密碼錯誤");
-//			} else {
-				// 導頁
-				//   window.location.href = url;
+			//			if (unlock === 'fail') {
+			//				document.getElementById('loadingOverlay').style.display = 'flex';
+			//			} else if (!url || url === 'null') {
+			//				alert("密碼錯誤");
+			//			} else {
+			// 導頁
+			//   window.location.href = url;
 
-				// 清空輸入框內容
-//				document.getElementById('pass-a').value = '';
-//				document.getElementById('pass-b').value = '';
-//				document.getElementById('pass-c').value = '';
-//				document.getElementById('pass-d').value = '';
+			// 清空輸入框內容
+			//				document.getElementById('pass-a').value = '';
+			//				document.getElementById('pass-b').value = '';
+			//				document.getElementById('pass-c').value = '';
+			//				document.getElementById('pass-d').value = '';
 
-				// 在新標籤頁中打開 URL
-//				window.open(url, '_blank');
-//				alert(url);
-//			}
+			// 在新標籤頁中打開 URL
+			//				window.open(url, '_blank');
+			//				alert(url);
+			//			}
 		})
 		.catch(error => {
 			// 隱藏 loading 畫面
@@ -74,9 +79,86 @@ document.getElementById('unlock-btn').addEventListener('click', function() {
 		});
 });
 
+// 送出贏家資料
+document.getElementById('winner-btn').addEventListener('click', function() {
+	// 顯示 loading 畫面
+	document.getElementById('loadingOverlay').style.display = 'flex';
+	document.getElementById('winner-card').style.display = 'none';
+	// 取得 input 的值
+	var winnerName = document.getElementById('winner-name').value.trim();
+	var winnerUuid = document.getElementById('winner-uuid').value.trim();
+
+	// 檢查每個 贏家名稱 是否有輸入
+	if (isValidStr(winnerName)) {
+		document.getElementById('winner-card').style.display = 'flex';
+		alert("請輸入名稱");
+		return;
+	}
+
+	// 檢查其他元素
+	if (isValidStr(winnerUuid)) {
+		document.getElementById('loadingOverlay').style.display = 'none';
+		alert("發生錯誤，請告知婚宴聯絡人");
+		return;
+	}
+
+	// 發送 POST 請求
+	fetch('http://localhost:8080/api/thief_winner', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			winnerName: winnerName,
+			winnerUuid: winnerUuid
+		})
+	})
+		.then(response => response.json())
+		.then(data => {
+			// 取出返回的值
+			const url = data.url;
+
+			// 隱藏 loading 畫面
+			document.getElementById('loadingOverlay').style.display = 'none';
+
+			// 檢查返回的值
+			if (isValidStr(url)) {
+				alert("發生錯誤，請告知婚宴聯絡人");
+			}
+			// 另開視窗
+			window.open(url, '_blank');
+
+			// 清空欄位
+			document.getElementById('pass-a').value = '';
+			document.getElementById('pass-b').value = '';
+			document.getElementById('pass-c').value = '';
+			document.getElementById('pass-d').value = '';
+			document.getElementById('pass-d').value = '';
+			document.getElementById('winner-name').value = '';
+			document.getElementById('winner-uuid').value = '';
+			return;
+		})
+		.catch(error => {
+			console.log('into erro')
+			// 隱藏 loading 畫面
+			document.getElementById('loadingOverlay').style.display = 'none';
+			console.error('Error:', error);
+			alert("發生錯誤，請告知婚宴聯絡人");
+		});
+});
+
 // 檢查 input 的值是否是一個數字且只有一個字元
 function isValidNumber(value) {
 	return value.length === 1 && /^[0-9]$/.test(value);
+}
+
+// 檢查 input 的值是否為空
+function isValidStr(value) {
+	if (typeof value === 'undefined' || value === null || value.length === 0) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 // 按下解密失敗按鈕後，關閉畫面
